cc0dfc1a4d0e230ddcfb5b6eeb4edbd7771abe74
pasupulaphani <pasupulaphani@gmail.com>
2015-03-07 22:33:50 +0000

    fixing init so it doesn't fail via navigation

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,39 +1,40 @@
 'use strict';
 
-function init() {
+var DEBUG = window.adbYtDebug || false;
 
-    var DEBUG = window.adbYtDebug || false;
-    DEBUG =true;
+var adbYtLog = function(msg) {
+    if (console && DEBUG) {
+        console.warn(msg);
+    }
+};
 
-    var adbYtLog = function (msg) {
-        if (console && DEBUG) {
-            console.warn(msg);
-        }
-    };
-    adbYtLog('inited');
+var player = document.querySelector('#player');
 
-    var videoAdContainer = document.getElementsByClassName('video-ads html5-stop-propagation')[0];
+function skipAds(e) {
+    adbYtLog('listener triggered');
 
-    function listener(e) {
-        adbYtLog('listener triggered');
-
-        if (e.target.innerHTML.length > 0 && document.getElementsByClassName('videoAdUi').length > 0) {
-            adbYtLog('skiping video ad');
-            document.getElementsByClassName('video-stream html5-main-video')[0].src = '';
-        }
-
-        var flashAdContainer = document.getElementsByClassName('ad-container ad-container-single-media-element-annotations')[0];
-        if (flashAdContainer && flashAdContainer.style.display !== 'none') {
-            adbYtLog('undisplay overlay ad');
-            flashAdContainer.style.display = 'none';
-        }
+    if (e.target.innerHTML.length > 0 && document.getElementsByClassName('videoAdUi').length > 0) {
+        adbYtLog('skiping video ad');
+        document.getElementsByClassName('video-stream html5-main-video')[0].src = '';
     }
 
-    if (videoAdContainer) {
-        videoAdContainer.addEventListener('DOMSubtreeModified', listener);
-    } else {
-        // wait for the url
+    var flashAdContainer = document.getElementsByClassName('ad-container ad-container-single-media-element-annotations')[0];
+    if (flashAdContainer && flashAdContainer.style.display !== 'none') {
+        adbYtLog('undisplay overlay ad');
+        flashAdContainer.style.display = 'none';
     }
 }
 
-init();
+function init() {
+
+    var videoAdContainer = document.getElementsByClassName('video-ads html5-stop-propagation')[0];
+
+    if (videoAdContainer) {
+
+        adbYtLog('inited');
+        player.removeEventListener('DOMSubtreeModified', init);
+        videoAdContainer.addEventListener('DOMSubtreeModified', skipAds);
+    }
+}
+
+player.addEventListener('DOMSubtreeModified', init);
